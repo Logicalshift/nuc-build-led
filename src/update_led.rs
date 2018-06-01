c8e5c714bfed7f06dff3daf9897970b5a4ddf43d
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-01 16:23:09 +0100

    Just use a standard FS write to update the LED file

--- src/update_led.rs
+++ src/update_led.rs
@@ -1,7 +1,7 @@
 use futures::*;
-use tokio::io;
-use tokio::fs::file::File;
-use std::io::Error;
+
+use std::fs::File;
+use std::io::{Error, Write};
 
 /// Proc file used to control the LED
 const LED_PROC_FILE: &str = "/proc/acpi/nuc_led";
@@ -13,11 +13,12 @@ const LED_PROC_FILE: &str = "/proc/acpi/nuc_led";
 pub fn update_led_state<Input: Stream<Item=String, Error=Error>>(input: Input) -> impl Stream<Item=(), Error=Error> {
     input.and_then(|led_string| {
         // Buffer the LED string for later writing
-        let led_string_buf = led_string.into_bytes();
+        let led_string_buf      = led_string.into_bytes();
 
-        // Every time we get an input, write it to the file
-        File::create(LED_PROC_FILE)
-            .and_then(move |led_proc_file| io::write_all(led_proc_file, led_string_buf))
-            .and_then(|_| Ok(()))
+        // Write straight to the control file in /proc
+        let mut led_proc_file   = File::create(LED_PROC_FILE)?;
+        led_proc_file.write_all(&led_string_buf)?;
+
+        Ok(())
     })
 }
