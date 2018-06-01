1481fd406559ca4f9f280202766cf7310b407382
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-01 12:28:28 +0100

    Add some code for creating a unix-domain socket and parsing anything sent there as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_json;
+extern crate futures;
+extern crate tokio;
+extern crate tokio_uds;
+extern crate tokio_core;
+
+mod led;
+mod led_socket;
+
 fn main() {
     println!("Hello, world!");
 }
